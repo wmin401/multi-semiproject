086a916f733bf9726fec1b649c26fc8dad8ea76f
woonggi_min@tmax.co.kr <52664613+wmin401@users.noreply.github.com>
2024-02-05 05:20:32 +0900

    complete

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,14 +1,12 @@
 # myapp/urls.py
 # myapp/urls.py
 from django.urls import path
-from .views import get_api_view, post_api_view, visualize_data
+from .views import naive_bayes
 from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
-    path('get-api/', get_api_view, name='get-api'),
-    path('post-api/', post_api_view, name='post-api'),
     path('static/<path:path>/', serve, {'document_root': settings.STATICFILES_DIRS[0]}),
     path('', serve, {'document_root': settings.STATICFILES_DIRS[0], 'path': 'index.html'}),
-    path('visualize/', visualize_data, name='visualize_data'),
+    path('naive_bayes/', naive_bayes, name='naive_bayes'),
 ]
