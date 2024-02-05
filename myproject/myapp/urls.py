c5e27240609e9ca70c9e9627d967633027236748
JUNWON <ozumiqq@gamil.com>
2024-02-05 22:52:35 +0900

    view

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,13 +1,14 @@
 # myapp/urls.py
 # myapp/urls.py
 from django.urls import path
-from .views import get_api_view, post_api_view
+from .views import naive_bayes
 from django.views.static import serve
 from django.conf import settings
+from .views import SupportVectorMachine
 
 urlpatterns = [
-    path('get-api/', get_api_view, name='get-api'),
-    path('post-api/', post_api_view, name='post-api'),
     path('static/<path:path>/', serve, {'document_root': settings.STATICFILES_DIRS[0]}),
     path('', serve, {'document_root': settings.STATICFILES_DIRS[0], 'path': 'index.html'}),
+    path('naive_bayes/', naive_bayes, name='naive_bayes'),
+    path('predict/', SupportVectorMachine.as_view(), name='predict'),
 ]
