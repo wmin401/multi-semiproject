c020b5096fbce0ecb5cb94850b02bdcb7e33300e
woonggi_min@tmax.co.kr <52664613+wmin401@users.noreply.github.com>
2024-02-06 21:56:23 +0900

    merged to CKH

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,6 @@
 # myapp/urls.py
 from django.urls import path
-from .views import naive_bayes, k_means_clustering, svm_function
+from .views import naive_bayes, k_means_clustering, svm_function, Linear_Regression, Logistic_Regression
 from django.views.static import serve
 from django.conf import settings
 
@@ -10,5 +10,8 @@ urlpatterns = [
     path('naive_bayes/', naive_bayes, name='naive_bayes'),
     path('kmeans/', k_means_clustering, name='k_means_clustering'),
     path('svm/', svm_function, name='svm'),
+    path('Linear_Regression/', Linear_Regression, name='Linear_Regression'),
+    path('Logistic_Regression/', Logistic_Regression, name='Logistic_Regression'),
 ]
+
 
