3757e24a76f1fbd67fbd6ffa49c075f00bc20ed1
임소혜 <huenell1411@naver.com>
2024-02-06 10:59:01 +0900

    RandomForest 최종본

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,7 @@
 # myapp/urls.py
 # myapp/urls.py
 from django.urls import path
-from .views import naive_bayes
+from .views import naive_bayes, RandomForest
 from django.views.static import serve
 from django.conf import settings
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('static/<path:path>/', serve, {'document_root': settings.STATICFILES_DIRS[0]}),
     path('', serve, {'document_root': settings.STATICFILES_DIRS[0], 'path': 'index.html'}),
     path('naive_bayes/', naive_bayes, name='naive_bayes'),
+    path('RandomForest/', RandomForest, name='RandomForest'),
 ]
